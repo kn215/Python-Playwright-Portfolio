d07803f09bdbba387e39457de2d9e67150cbd1a9
kn215 <punch1inerock@gmail.com>
2026-07-06 10:26:15 -0400

    added test for wrong password

--- tests/test_login.py
+++ tests/test_login.py
@@ -14,3 +14,10 @@ def test_invalid_login(page):
     login_page.login("wrong_user", "wrong_pass")
 
     assert page.get_by_text("Epic sadface: Username and password do not match").is_visible()
+
+def test_wrong_password(page):
+    login_page = LoginPage(page)
+    login_page.goto()
+    login_page.login("standard_user", "wrong_pass")
+
+    assert page.get_by_text("Epic sadface: Username and password do not match").is_visible()
